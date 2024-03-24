6fa14b66fd105043924de44241cc1464299480fa
Emmahchinonso <emmanuelchinons9@gmail.com>
2024-03-24 23:29:24 +0100

    update urql cache to use graphcache

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,19 +1,42 @@
 "use client";
 import { Link } from "@chakra-ui/next-js";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import { useMeQuery } from "../generate/graphql";
 
 const Navbar = () => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml="auto">
+  const [{ data, fetching }] = useMeQuery();
+
+  let body: any;
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    body = (
+      <>
         <Link href="/login" color="white" mr={4}>
           Login
         </Link>
         <Link href="/register" color="white">
           Register
         </Link>
-      </Box>
+      </>
+    );
+  } else {
+    body = (
+      <Flex gap={3}>
+        <Text fontSize="md" color="black" fontWeight="medium">
+          {data?.me.username}
+        </Text>
+        <Button variant="link" color="black">
+          Logout
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tan" p={4}>
+      <Box ml="auto">{body}</Box>
     </Flex>
   );
 };
--- app/provider/providers.tsx
+++ app/provider/providers.tsx
@@ -9,13 +9,52 @@ import { theme } from "../theme";
 import {
   Provider as URQLProvider,
   Client,
-  cacheExchange,
   fetchExchange,
+  Exchange,
 } from "urql";
+import { cacheExchange } from "@urql/exchange-graphcache";
+import {
+  MeDocument,
+  LoginMutation,
+  MeQuery,
+  RegisterMutation,
+} from "../generate/graphql";
+import { devtoolsExchange } from "@urql/devtools";
 
 const client = new Client({
   url: "http://localhost:4000/graphql",
-  exchanges: [cacheExchange, fetchExchange],
+  exchanges: [
+    devtoolsExchange as Exchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login(result: LoginMutation, args, cache, info) {
+            cache.updateQuery({ query: MeDocument }, (data: MeQuery | null) => {
+              if (result.login.errors) {
+                return data;
+              }
+              return {
+                ...data,
+                me: result.login.user,
+              };
+            });
+          },
+          register(result: RegisterMutation, args, cache, info) {
+            cache.updateQuery({ query: MeDocument }, (data: MeQuery | null) => {
+              if (result.register.errors) {
+                return data;
+              }
+              return {
+                ...data,
+                me: result.register.user,
+              };
+            });
+          },
+        },
+      },
+    }) as Exchange,
+    fetchExchange,
+  ],
   fetchOptions: {
     credentials: "include",
     headers: {
